1e2758de7ab89ac6f06cf44a52f13443b99e5f55
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-21 10:46:05 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TloSrodekComponent } from './tlo-srodek/tlo-srodek.component';
+import { PanelLewyComponent } from './panel-lewy/panel-lewy.component';
+import { PanelPrawyGlownyComponent } from './panel-prawy-glowny/panel-prawy-glowny.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TloSrodekComponent,
+    PanelLewyComponent,
+    PanelPrawyGlownyComponent
   ],
   imports: [
     BrowserModule
