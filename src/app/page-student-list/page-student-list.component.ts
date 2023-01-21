cf889a0bfe5e7f160e60fd0af8841614e7268eb6
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-21 15:19:09 +0100

    Typescript

--- src/app/page-student-list/page-student-list.component.ts
+++ src/app/page-student-list/page-student-list.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export type StudentModel = {
+  lp: number,
+  imie: string,
+  nazwisko: string,
+  dataUrodzenia: string
+}
 
 @Component({
   selector: 'app-page-student-list',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-student-list.component.css']
 })
 export class PageStudentListComponent {
+  studentList: StudentModel[] = [];
 
+  constructor() {
+    this.studentList = [
+      {
+        lp: 1,
+        imie: 'Pawel',
+        nazwisko: 'Gawel',
+        dataUrodzenia: '2022-01-05'
+      },
+
+    ]
+  }
 }
