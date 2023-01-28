0d17833ddaaccd6693ae4dbe0647db634689c31b
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-28 12:26:33 +0100

    Form - dodawanie działa

--- src/app/student-service/student.service.ts
+++ src/app/student-service/student.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {StudentModel} from "../model/StudentModel";
 
 @Injectable({
@@ -9,11 +9,19 @@ export class StudentService {
 
   constructor() {
     // push ==> add()
-    this.studentList.push({
-      lp: 1,
-      imie: 'pawel',
-      nazwisko: 'gawel',
-      dataUrodzenia: '2022-01-01'
-    })
+    // this.studentList.push({
+    //   id: 1,
+    //   imie: 'pawel',
+    //   nazwisko: 'gawel',
+    //   dataUrodzenia: '2022-01-01'
+    // })
+  }
+
+  // nazwa metody   => dodajStudenta
+  // nowyStudent    => nazwa parametru
+  // :StudentModel  => typ parametru
+  // :void          => typ zwracany funkcji
+  dodajStudenta(nowyStudent: StudentModel): void {
+    this.studentList.push(nowyStudent)
   }
 }
