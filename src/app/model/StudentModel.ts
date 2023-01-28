2e158e253c603c6d0f041e175db72021526c3b35
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-28 11:20:29 +0100

    Service i form

---
+++ src/app/model/StudentModel.ts
@@ -0,0 +1,8 @@
+// export ==> public
+// type StudentModel ==> class StudentModel {}
+export type StudentModel = {
+  lp: number
+  imie: string
+  nazwisko: string
+  dataUrodzenia: string
+}
---
+++ src/app/student-service/student.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {StudentModel} from "../model/StudentModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  studentList: StudentModel[] = []
+
+  constructor() {
+    // push ==> add()
+    this.studentList.push({
+      lp: 1,
+      imie: 'pawel',
+      nazwisko: 'gawel',
+      dataUrodzenia: '2022-01-01'
+    })
+  }
+}
